59c10637964cf4352b4f40a83af0dcb0c3563f0a
Alberto Morini <albydev99@gmail.com>
2019-12-15 20:06:12 +0100

    add folder "appunti" and design of project

---
+++ server/headerServer.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/un.h> // per struct sockaddr_un
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
---
+++ server/server.c
@@ -0,0 +1,6 @@
+#include <headerServer.h>
+#include <common.h>
+
+int main(){
+    
+}
